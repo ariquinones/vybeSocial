3b6ed3281b614bd45dc2364134f94e44c8f5d3a8
ari.quinones <ari.quinones@remuv.io>
2016-07-30 21:23:07 -0500

    websiteLaunch

--- js/script.js
+++ js/script.js
@@ -2,3 +2,12 @@ document.querySelector('button').addEventListener('click', function() {
 	var input = document.querySelector('input')
 	input.value = ''
 })
+
+document.querySelector(".showMobileMenu").addEventListener('change', function() {
+	if (document.querySelector(".showMobileMenu").checked == true) {
+		document.querySelector(".mobileMenu").style.right = "5px"
+	}
+	if (document.querySelector(".showMobileMenu").checked == false) {
+		document.querySelector(".mobileMenu").style.right = "-50%"
+	}
+})
